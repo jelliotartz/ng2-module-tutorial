29bb6204158bc08354fa79a7a9fd34d78b9ca4b9
jelliotartz <jelliotartz@gmail.com>
2016-09-25 14:05:34 -0400

    Add highlight directive to change background color.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
-import
-       { AppComponent }  from './app.component'
+import { HighlightDirective } from './highlight.directive'
+import { AppComponent }  from './app.component'
 
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HighlightDirective
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
