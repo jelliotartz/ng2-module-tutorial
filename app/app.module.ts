2d428f13f90b5dc5be54801bf87d6b0dc461b192
jelliotartz <jelliotartz@gmail.com>
2016-10-03 20:08:44 -0500

    Create feature module for Contact, reorganize root module.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,42 +1,29 @@
 import { NgModule }      from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
 
-import { HighlightDirective } from './contact/highlight.directive'
 import { AppComponent }  from './app.component'
+import { HighlightDirective } from './highlight.directive'
 import { TitleComponent } from './title.component'
-
-import { AwesomePipe } from './contact/awesome.pipe'
-import { ContactComponent } from './contact/contact.component'
-import { HighlightDirective as ContactHighlightDirective } from './contact/highlight.directive'
-
 import { UserService } from './user.service'
-import { ContactService } from './contact/contact.service'
+
+import { ContactModule } from './contact/contact.module'
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule,
+    ContactModule,
   ],
   declarations: [
     AppComponent,
     HighlightDirective,
     TitleComponent,
-    AwesomePipe,
-    ContactComponent,
-    ContactHighlightDirective,
   ],
   providers: [
     UserService,
-    ContactService,
   ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
-
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
